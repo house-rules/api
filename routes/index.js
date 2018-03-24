282fc88fff69aa4dd2cfe23005d17406dcdc60de
Joseph Gordy <jgordy2424@gmail.com>
2018-03-23 23:08:50 -0400

    Created basic layouts of different routes needed

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ const router = express.Router();
 
 
 router.get("/", function(req, res) {
-  res.render("index");
+  console.log('<----get @ /---->');
+  console.log('req---> ', req);
+  res.status('200').send("Hiya");
+});
+
+router.post("/", function(req, res) {
+  console.log('<----post @ /---->');
+  console.log('req---> ', req.body);
+  res.status('200').send(req.body);
+});
+
+router.post('/login', function(req, res) {
+  console.log('<----post @ /login ---->');
+  console.log('req---> ', req.body);
+  // console.log('res---> ', res.body);
+  res.status("200").send(req.body);
+});
+
+router.post('/signup', function(req, res) {
+  console.log('<----post @ /signup ---->');
+  console.log('req---> ', req.body);
+  res.status('200').send(req.body);
 });
 
 
