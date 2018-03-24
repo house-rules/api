f3d40f73cbd8d28a63f83adbe1a36d856d597fb7
Joseph Gordy <jgordy2424@gmail.com>
2018-03-24 14:48:16 -0400

    Used sequelize init to create the models, config, and migrations directories. Update the routes and added bcrypt for password hashing

--- routes/index.js
+++ routes/index.js
@@ -2,34 +2,110 @@ const express         = require("express");
 const router          = express.Router();
 const models          = require("../models/index");
 const passport        = require('passport');
+const bcrypt          = require("bcrypt");
 const BasicStrategy   = require('passport-http').BasicStrategy;
 
 
 router.get("/", function(req, res) {
   console.log('<----get @ /---->');
   console.log('req---> ', req);
-  res.status('200').send({status: "200", message: 'Everything is fine, we\'re fine', requestBody: req.body});
+  res.status(200).send({status: "200", message: 'Everything is fine, we\'re fine', requestBody: req.body});
 });
 
 router.post("/", function(req, res) {
   console.log('<----post @ /---->');
   console.log('req/---> ', req.body);
-  res.status('200').send(req.body);
+  res.status(200).send(req.body);
 });
 
 // login route
 router.post('/login', function(req, res) {
   console.log('<----post @ /login ---->');
   console.log('req/login---> ', req.body);
-  res.status("200").send(req.body);
+  res.status(200).send(req.body);
 });
 
 // signup route
 router.post('/signup', function(req, res) {
   console.log('<----post @ /signup ---->');
   console.log('req/signup---> ', req.body);
-  res.status('200').send(req.body);
+
+  let username = req.body.username
+  let password = req.body.password
+  let email     = req.body.email
+  let confirmPassword = req.body.confirmPassword
+
+  if (!username || !password) {
+    res.status(403).send('User name and password must not be blank.')
+  }
+
+  let salt = bcrypt.genSaltSync(10)
+  let passwordHash = bcrypt.hashSync(password, salt)
+
+  let newUser = {
+    email: email,
+    username: username,
+    salt: salt,
+    password: passwordHash
+  }
+
+  // if (password === confirmPassword) {
+  //   models.User.create(newUser)
+  //   .then(function(data) {
+  //     res.status('201').send({status: 'success', data: data});
+  //   })
+  //   .catch(function(error) {
+  //     res.status('400').send({status: 'failed', error: error});
+  //   });
+  // } else {
+  //   res.status('').send({status: "failed", error: "Passwords to not match."})
+  // }
+
+  res.status(200).send(newUser);
 });
+
+// getting the entire gamelist
+router.get('/gameList', function(req, res) {
+  console.log('<----get @ /gameList ---->');
+  console.log('req/gameList---> ', req.body);
+
+  // models.Game.findAll({})
+  // .then(function(data) {
+  //   if (data) {
+  //     data = {"status": "success", data: data};
+  //     res.status(200).json(data);
+  //   } else {
+  //     res.status(404).send("Data not found")
+  //   }
+  // })
+  // .catch(function(err) {
+  //   err = {"status": "fail", error: err};
+  //   res.status(500).json(err);
+  // })
+  res.status(200).send({status: 'success', message: "This is a placeholder for the gamelist until the db is created"});
+})
+
+// getting a game based on id
+router.get('/game/:id', function(req, res) {
+  console.log('<----get @ /gameList ---->');
+  console.log('req/game/:id request params---> ', req.params);
+  console.log('req/game/:id request body---> ', req.body);
+  res.status(200).send({status: 'success', message: "This will one day be an actual games details", gameId: req.params.id});
+})
+
+// create a game with the details
+
+
+// delete a game based on id
+// dont forget to delete associated data with it (Alternates and Likes)
+
+
+// create an alternate version of a game based on gameId
+
+
+// delete an alternate version of a game based on gameId and alternateId
+
+
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path              = require("path");
 const routes            = require("./routes/index");
 const morgan            = require("morgan");
 const passport          = require('passport');
+const bcrypt            = require("bcrypt");
 const BasicStrategy     = require('passport-http').BasicStrategy;
 
 const app = express();
@@ -22,6 +23,49 @@ app.engine("mustache", mustacheExpress());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(morgan("dev"));
+
+app.use(passport.initialize());
+
+const authenticateUser = function(username, password, done) {
+  model.User.findOne({
+    where: {
+      'username': username.toLowerCase()
+    }
+  }).then(function (user) {
+    if (user == null) {
+      return done(null, false, { message: 'Invalid email and/or password: please try again' })
+    }
+
+    let hashedPassword = bcrypt.hashSync(password, user.salt)
+
+    if (user.password === hashedPassword) {
+      return done(null, user)
+    }
+
+    return done(null, false, { message: 'Invalid email and/or password: please try again' })
+  })
+}
+
+passport.use(new BasicStrategy(authenticateUser))
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id)
+})
+
+passport.deserializeUser(function(id, done) {
+  model.User.findOne({
+    where: {
+      'id': id
+    }
+  }).then(function (user) {
+    if (user == null) {
+      done(new Error('Wrong user id'))
+    }
+
+    done(null, user)
+  })
+})
+
 
 app.use(routes);
 
