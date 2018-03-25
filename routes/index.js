0af1e92faa3372144ff98da6175711a0ab3143e2
Joseph Gordy <jgordy2424@gmail.com>
2018-03-25 14:04:40 -0400

    Returning the req.body in the login path for debugging

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,7 @@ router.post("/", function(req, res) {
 router.post('/login', function(req, res) {
   console.log("req.body---->>>> ",req.body);
   if ((!req.body.username) || (!req.body.password)) {
-    res.status(403).send('Fields must not be empty.')
+    res.status(403).send('Fields must not be empty.', req.body)
   } else {
     models.User.findOne({
       where: {
