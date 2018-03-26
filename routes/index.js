4f9fac63948e7ae6fe97a61416797f1e0f234aab
Joseph Gordy <jgordy2424@gmail.com>
2018-03-26 08:45:16 -0400

    Added cors enabler in routes.js

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,12 @@ passport.use(new BasicStrategy(
   }
 ));
 
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 router.get("/", function(req, res) {
   console.log('<----get @ /---->');
